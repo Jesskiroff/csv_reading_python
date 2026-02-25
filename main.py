005f47cf452d9266d416d89de648d0ce4bc17892
Jessica Kiroff <jesskiroff@gmail.com>
2026-02-24 19:10:36 -0500

    add temperature to list as list items

--- main.py
+++ main.py
@@ -6,5 +6,14 @@ import csv
 
 with open("./weather_data.csv") as data_file:
     data = csv.reader(data_file)
+    temperature = []
     for row in data:
-        print(row)
+       if row[1] != "temp":
+        temperature.append(row[1])
+
+    print(temperature)
+       
+      
+    #     temperature += temp
+    # print(temperature)
+       
