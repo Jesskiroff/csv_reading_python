e3ed042b4522d0c3ce2d563a21aeb7f272c0bc15
Jessica Kiroff <jesskiroff@gmail.com>
2026-02-25 15:23:49 -0500

    print gray squirrel count

--- main.py
+++ main.py
@@ -20,17 +20,17 @@
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv") 
+# data = pandas.read_csv("weather_data.csv") 
 # print(type(data)) # There's a series and dataframe data type in pandas
 #the whole table is the whole file and a series is one particula column
 # temperature = data ["temp"]
 # print(temperature)# Should print the pandas version
 
-data_dict = data.to_dict()
+# data_dict = data.to_dict()
 # print(data_dict)
 
-temp_list = data["temp"].to_list()
-number_of_temps_calculated = len(temp_list)
+# temp_list = data["temp"].to_list()
+# number_of_temps_calculated = len(temp_list)
 # print(temp_list)
 # print(number_of_temps_calculated)
 
@@ -38,23 +38,30 @@ number_of_temps_calculated = len(temp_list)
 
 # print(average_temp)
 
-average_temp = data["temp"].mean()
-max_temp = data["temp"].max()
+# average_temp = data["temp"].mean()
+# max_temp = data["temp"].max()
 # print(average_temp , max_temp)
 
 
 # print(data[data.day == "Monday"])
 # print(data[data.temp == data["temp"].max()])
 
-monday = data[data.day =="Monday"]
+# monday = data[data.day =="Monday"]
 # print(monday.condition)
 # print(monday.temp)
 
-data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 65]
-}
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
 
-create_dataframe = pandas.DataFrame(data_dict)
-create_dataframe.to_csv("new_dataframe.csv")
+# create_dataframe = pandas.DataFrame(data_dict)
+# create_dataframe.to_csv("new_dataframe.csv")
 # print(create_dataframe)
+
+# ASSIGNMENT - create csv that's called squirrel_count.csv  
+# Figure out how many grey, cinnamon, and black squirrels there are
+
+squirrel_data = pandas.read_csv("squirrel_data.csv")
+gray_squirrel = squirrel_data[squirrel_data["Primary Fur Color"]== "Gray"]
+print(len(gray_squirrel))
