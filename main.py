244bf684a5418ab14573c2ac8c9d85ed035af289
Jessica Kiroff <jesskiroff@gmail.com>
2026-02-24 19:26:24 -0500

    add csv temp data into list as ints

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ with open("./weather_data.csv") as data_file:
     temperature = []
     for row in data:
        if row[1] != "temp":
-        temperature.append(row[1])
+        temperature.append(int(row[1]))
 
     print(temperature)
        
